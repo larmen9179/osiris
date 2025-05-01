b521e7e9e9535cfe7b0d84a8d21af3d88a54c94e
larmen9179 <lo.j.armendariz@gmail.com>
2025-04-30 19:03:34 -0600

    removed old projectiles scenes for testing

--- osiris/character/Isis/playerScript.gd
+++ osiris/character/Isis/playerScript.gd
@@ -8,52 +8,78 @@ extends CharacterBody2D
 #scarab orb
 @export var scarab_scene: PackedScene
 #ra orb
-@export var ra_scene: PackedScene
+#@export var ra_scene: PackedScene
 #horus orb
-@export var horus_scene: PackedScene
+#@export var horus_scene: PackedScene
 
 #player's speed
 @export var speed: int = 100
-#-----------------------TESTING-----------------------
-@export var shoot_cooldown = .3
-@onready var random = RandomNumberGenerator.new()
+
+#cooldown before next shot can be fired
+@export var shoot_cooldown: float = .3
+
+#random generator for random magic attack
+@onready var random: RandomNumberGenerator = RandomNumberGenerator.new()
+
+#storing the last direction as down so the first shot moves
 var last_direction: Vector2 = Vector2.DOWN
-var shoot_timer = 0.0
-#-----------------------------------------------------
+
+#timer for the shot cooldown
+var shoot_timer: float = 0.0
+
+#boolean for attack state
+var attacking: bool = false
 
 
 
 func _physics_process(delta):
-	#grabbing 
+	#grabbing the Vector 2 that tells us what keys the user was pressing
+	#for movement
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#adjusting the velocity based on direction and a constant speed
 	velocity = direction * speed
 	
-	last_direction = direction
-
+	#only updating the last direction if we're actually moving
+	#updating this WITHOUT boundary checks will make the orbs stand still
+	if abs(direction.x) > 0 || abs(direction.y) > 0:
+		last_direction = direction
+	
+	#moves the body based off the velocity changed earlier
 	move_and_slide()
+	
+	#plays animation based on movement
+	play_animation()
+	
+	#reduces the shoot timer if the timer has been reset for an attack
+	if shoot_timer > 0:
+		shoot_timer -= delta  
+	
 
-	if velocity.length() > 0:
-		if abs(velocity.x) > abs(velocity.y):
-			if velocity.x > 0:
+#detects input
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_K:
+			#spell cast method
+			use_spell()
+
+#method for playing animation given the users previously pressed direction
+func play_animation():
+	
+		if abs(last_direction.x) > abs(last_direction.y):
+			if last_direction.x > 0:
 				$AnimatedSprite2D.play("right")
 			else:
 				$AnimatedSprite2D.play("left")
 		else:
-			if velocity.y > 0:
+			if last_direction.y > 0:
 				$AnimatedSprite2D.play("down")
 			else:
 				$AnimatedSprite2D.play("up")
-	else:
-		$AnimatedSprite2D.stop()
-	
-	shoot_timer -= delta  
-	
 
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_K:
-			use_spell()
-
+#temporary function header
+#func play_attack_animation():
+	
+# to cast spell
 func use_spell():
 	
 	if shoot_timer > 0:
@@ -79,6 +105,7 @@ func use_spell():
 	else:
 		attack_sound_2.play()
 
+	#spawning specific spell
 	var spell_number = random.randi_range(1, 3)
 	match spell_number:
 		1:
@@ -86,13 +113,14 @@ func use_spell():
 			summon_spell_orb(scarab_scene)
 		2:
 			print("Isis summons the Eye of Horus! (Medium Damage)")
-			summon_spell_orb(horus_scene)
+			summon_spell_orb(scarab_scene)
 		3:
 			print("Isis calls upon Ra the Sun God! (Strongest Damage)")
-			summon_spell_orb(ra_scene)
+			summon_spell_orb(scarab_scene)
 
 func summon_spell_orb(spell_scene: PackedScene):
-	var orb = spell_scene.instantiate()
+	var orb = scarab_scene.instantiate()
 	get_parent().add_child(orb)
 	orb.global_position = global_position
 	orb.direction = last_direction
+	
